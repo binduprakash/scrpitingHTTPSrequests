7b9f3766fb2049100b72477fcafb25987d385719
Bindu Prakash <bindu2305@gmail.com>
2019-02-12 06:44:45 +0000

    step 6 completed

---
+++ callbacks/http-functions.js
@@ -0,0 +1,16 @@
+var https = require('https');
+module.exports = function getHTML (options, callback) {
+  /* Your code here */
+  var buffer = '';
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      buffer += ('Data:' + data + '\n');
+    });
+
+    response.on('end', function() {
+      callback(buffer);
+    });
+  });
+}
