589a5e13a473cf11f4ed355a6979d4e0f999e4c3
Bindu Prakash <bindu2305@gmail.com>
2019-02-12 06:00:08 +0000

    step 3 completed

---
+++ getAndPrintHTML2.js
@@ -0,0 +1,26 @@
+var https = require('https');
+function getAndPrintHTML (options) {
+
+
+
+  /* Add your code here */
+  var buffer = '';
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+    buffer += ('Data:' + data + '\n');
+  });
+
+     response.on('end', function() {
+    console.log(buffer);
+  });
+
+});
+
+}
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+getAndPrintHTML(requestOptions);
